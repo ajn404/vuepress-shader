65f1fca7234c8caa613336772c3220b9bd6408c4
ajn404 <huiyuening00@gmail.com>
2022-08-05 17:06:21 +0800

    no message

--- src/.vuepress/config/head.config.ts
+++ src/.vuepress/config/head.config.ts
@@ -2,7 +2,7 @@
 import { HeadConfig } from "vuepress"
 
 const head: HeadConfig []= [
-    ['link', { rel: 'icon', href: 'images/logo.jpg' }]
+    ['link', { rel: 'icon', href: '/images/logo.jpg' }]
 ]
 
 export default head;
