5bd502433cc862d08742ce15e3d51d5cd525489c
ajn404 <huiyuening00@gmail.com>
2022-08-29 15:17:22 +0800

    add ali icon

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -7,7 +7,10 @@ import ParentLayout from "@vuepress/theme-default/lib/client/layouts/Layout.vue"
         <template #navbar-before></template>
         <template #navbar-after></template>
         <template #page-bottom></template>
-        <template #page-top></template>
+        <template #page-top>
+                <!-- <span class="iconfont icon-yangshi_icon_tongyong_call">
+                </span> -->
+        </template>
     </ParentLayout>
 </transition>
 </template>
--- src/components/home.vue
+++ src/components/home.vue
@@ -5,10 +5,17 @@
 </template>
 <script lang="ts" setup>
 //setup 执行时dom还不存在
-import { 
-    ref, 
+import {
+    ref,
     computed,
+    watch,
 
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted
 } from 'vue';
 const data = ref([{
     value: '1', hidden: false
@@ -17,11 +24,52 @@ const computeData = computed(() => {
     return data.value.filter(item => item.hidden === false)
 })
 
+watch(data, (newVal) => {
+    console.log('data changed new data is ', data)
+})
+
 const method = () => {
     console.log('i am fool');
 }
-//https://cn.vuejs.org/guide/essentials/lifecycle.html#lifecycle-diagram
+
+//ts语法
+const props = defineProps<{
+    title?: String,
+    msg?: String,
+
+}>()
+
+
+//整个生命周期
 //setup=>beforeCreate=>created=>beforeMount=>mouted=>beforeUpdate=>updated=>beforeUnmount=>unmounted
 
+
+onMounted(() => {
+    console.log('onMounted')
+})
+
+onBeforeMount(() => {
+    console.log('onBeforeMount')
+})
+
+onBeforeUpdate(() => {
+    console.log('onBeforeUpdate')
+})
+
+onUpdated(() => {
+    console.log('onUpdated')
+})
+
+onBeforeUnmount(() => {
+    console.log('onBeforeUnmount')
+})
+
+onUnmounted(() => {
+    console.log('onUnmounted')
+})
+
+
+
+console.log('setup')
 
 </script>
