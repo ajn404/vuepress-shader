6c0cad297561bb26ce3bbd8c08ab772cb0cf85d0
ajn404 <huiyuening00@gmail.com>
2022-08-29 10:19:07 +0800

    template home

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,3 +3,25 @@
         h
     </div>
 </template>
+<script lang="ts" setup>
+//setup 执行时dom还不存在
+import { 
+    ref, 
+    computed,
+
+} from 'vue';
+const data = ref([{
+    value: '1', hidden: false
+}]);
+const computeData = computed(() => {
+    return data.value.filter(item => item.hidden === false)
+})
+
+const method = () => {
+    console.log('i am fool');
+}
+//https://cn.vuejs.org/guide/essentials/lifecycle.html#lifecycle-diagram
+//setup=>beforeCreate=>created=>beforeMount=>mouted=>beforeUpdate=>updated=>beforeUnmount=>unmounted
+
+
+</script>
