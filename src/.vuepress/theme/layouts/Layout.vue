043b733fd11474a6c61dfcd98edfe68e8db76335
ajn404 <huiyuening00@gmail.com>
2022-08-29 16:54:05 +0800

    无用功

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -1,16 +1,46 @@
+
+<template>
+    <transition name="fade" mode="out-in" appear>
+        <ParentLayout class="parent-layout">
+            <template #navbar-before></template>
+            <template #navbar-after>
+
+            </template>
+            <template #page-bottom>
+
+            </template>
+            <template #page-top>
+                <!-- <img src="/images/sun.svg" ref="img" @click.prevent="hideNavbar" :class="classList"> -->
+            </template>
+        </ParentLayout>
+    </transition>
+</template>
 <script setup lang="ts">
 import ParentLayout from "@vuepress/theme-default/lib/client/layouts/Layout.vue";
+import { ref } from "vue";
+const classList = ref(['ClockWise', 'img'])
+const img = ref(null)
+
+
+const hideNavbar = () => {
+    const body:HTMLElement=  document.querySelector('body');
+    const navbar:HTMLElement = document.querySelector('.parent-layout>.navbar')
+    if (!classList.value.includes('CounterClockWise')){
+        classList.value.push('CounterClockWise')
+        body.style.setProperty('--navbar-height','0');
+        navbar.style.setProperty('z-index','-1')
+    }
+    else {
+        body.style.setProperty('--navbar-height','3.6rem');
+        navbar.style.setProperty('z-index','20')
+        classList.value.pop()
+    }
+}
+
 </script>
-<template>
-<transition name="fade" mode="out-in" appear>
- <ParentLayout class="parent-layout">
-        <template #navbar-before></template>
-        <template #navbar-after></template>
-        <template #page-bottom></template>
-        <template #page-top>
-                <span class="iconfont icon-nav_drawer rotate">
-                </span>
-        </template>
-    </ParentLayout>
-</transition>
-</template>
+<style lang="scss">
+.img {
+    width: 4em;
+    position: fixed;
+}
+</style>
