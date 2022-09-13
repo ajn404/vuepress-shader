59cafb2974d9e420c99c2c4669ca66949f777d91
ajn404 <huiyuening00@gmail.com>
2022-09-13 13:02:14 +0800

    using step draw square

--- src/shaders/practice/usingStepDrawSquare/fragment.glsl
+++ src/shaders/practice/usingStepDrawSquare/fragment.glsl
@@ -7,9 +7,24 @@ uniform sampler2D iChannel1;
 uniform int u_frame;
 varying vec3 v_position;
 
+
+float rect(vec2 pt, vec2 size ,vec2 center){
+  vec2 p = pt - center;
+  vec2 halfSize = size * 0.5;
+  float horz = step(-halfSize.x, p.x ) - step(halfSize.x,p.x ) ;
+  float vert = step(-halfSize.y,p.y) - step(halfSize.y,p.y);
+  return horz*vert;
+}
+
+// rect(v_position.xy,vec2(1.0),vec2(0.0))
+
+
+
 void main(){
   float inRect = rect(v_position.xy,vec2(1.0),vec2(0.0));
+
   float inCircle = 1.0 - step(1.0,length(v_position.xy));
   vec3 color = vec3(0.4157, 0.2941, 0.2941) * inCircle;
+  // vec3 color = vec3(0.4157, 0.2941, 0.2941) * inRect;
   gl_FragColor=vec4(color,1.0);
 }
